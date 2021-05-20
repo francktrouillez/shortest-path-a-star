3afb5060315dd21450cdb9482e6b7e49308aa103
neji601 <>
2021-05-20 21:06:33 +0200

    Chnages done to generate closed graph

--- mainGenerator.py
+++ mainGenerator.py
@@ -2,5 +2,5 @@ from InstancesGenerator import InstancesGenerator
 
 
 if __name__ == "__main__":
-    instanceGenerator = InstancesGenerator(numberOfNodes = 2000, minX = 0, minY = 0, maxX = 300, maxY = 300, probabilityOfStrangeMove = 0.00, minConnexions = 1, maxConnexions = 10, minRadius = 5, maxRadius = 10)
+    instanceGenerator = InstancesGenerator(numberOfNodes = 1000, minX = 0, minY = 0, maxX = 300, maxY = 300, probabilityOfStrangeMove = 0.00, minConnexions = 1, maxConnexions = 10, minRadius = 2, maxRadius = 10)
     instanceGenerator.generate("testInstance.txt")

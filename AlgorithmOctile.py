e580f8d792c7da844ebd5e06613b0967d2f9b1cb
Kcnarf-exe <francktrouillez99@gmail.com>
2021-06-02 22:33:34 +0200

    oct

--- AlgorithmOctile.py
+++ AlgorithmOctile.py
@@ -12,9 +12,5 @@ class AlgorithmOctile(Algorithm):
         dx = abs(node_a[0] - node_b[0])
         dy = abs(node_a[1] - node_b[1])
         #Compute heuristic
-
-
-        return (dx + dy) + (sqrt(2) - 2) * min(dx, dy)
-        
-        #return (dx + dy) + self.CONSTANT_OCTILE * min(dx, dy)
+        return (dx + dy) + self.CONSTANT_OCTILE * min(dx, dy)
 
